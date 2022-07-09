dc0750a18ab31ffda887e2dfe2feaee48657e19d
JeffersonMira <jefferson.s.mira@hotmail.com>
2022-07-09 23:31:46 +0100

    Adding validations and customFormValidator

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { TaskService } from 'src/app/shared/task-service.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css', '../tasks.component.css']
 })
 export class AddTaskComponent implements OnInit {
 
+  //This is one way to declare the formGroup. I used the FormBuilder at editTask
   taskForm = new FormGroup({
     title: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required)
+  }, {
+    validators : this.controlTitleAndDescriptionAreEqual('title','description')
   });
 
   constructor(private taskService : TaskService,
@@ -30,5 +33,18 @@ export class AddTaskComponent implements OnInit {
     }
   }
 
+  //Creating my own validator
+  private controlTitleAndDescriptionAreEqual(controlNameA: string, controlNameB: string): ValidatorFn {
+    return (control : AbstractControl) : ValidationErrors | null => {
+      const formGroup = control as FormGroup
+      const valueOfControlA = formGroup.get(controlNameA)?.value
+      const valueOfControlB = formGroup.get(controlNameB)?.value
 
+      if(valueOfControlA == "" || valueOfControlB == "" || (valueOfControlA !== valueOfControlB)){
+        return null
+      }else{
+        return { valuesAreEqual : true }
+      }
+    }
+  }
 }
--- src/app/tasks/edit-task/edit-task.component.ts
+++ src/app/tasks/edit-task/edit-task.component.ts
@@ -8,7 +8,7 @@ import {first} from "rxjs";
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
-  styleUrls: ['./edit-task.component.css']
+  styleUrls: ['./edit-task.component.css', '../tasks.component.css']
 })
 export class EditTaskComponent implements OnInit {
 
