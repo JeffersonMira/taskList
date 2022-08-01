d854eda453f008a0f09cc3d5e75990553af3f1c2
JeffersonMira <jefferson.s.mira@hotmail.com>
2022-08-01 21:30:08 +0100

    Adding unsubscribe and small adjustments

--- src/app/tasks/edit-task/edit-task.component.ts
+++ src/app/tasks/edit-task/edit-task.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import { Status } from 'src/app/shared/task';
 import { TaskService } from 'src/app/shared/task-service.service';
-import {first} from "rxjs";
+import {first, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
   styleUrls: ['./edit-task.component.css', '../tasks.component.css']
 })
-export class EditTaskComponent implements OnInit {
+export class EditTaskComponent implements OnInit, OnDestroy {
 
   taskForm: FormGroup
   id: number
+  subscription : Subscription;
 
   status = Status;
   statusKeys : String[] = []
@@ -25,6 +26,10 @@ export class EditTaskComponent implements OnInit {
       this.statusKeys = Object.keys(this.status)
   }
 
+  ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.taskForm = this.formBuilder.group({
@@ -35,7 +40,7 @@ export class EditTaskComponent implements OnInit {
     });
 
     //Inserting the value into page's form
-    this.service.getTask(this.id).subscribe(
+    this.subscription = this.service.getTask(this.id).subscribe(
       data => {
         data != undefined ? this.taskForm.patchValue(data): ""
       });
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Task } from '../shared/task';
 import { TaskService } from '../shared/task-service.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit, OnDestroy{
 
   title = 'TODO list';
   taskList: Task[] = []
 
+  subscription : Subscription;
 
   constructor(private taskService : TaskService){
     console.log("This module is loaded")
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
   ngOnInit(): void {
-    this.refreshList()
+      this.subscription = this.taskService.getTasks()
+        .subscribe(t => this.taskList = t);
   }
 
   removeTask(index: number) {
@@ -28,8 +35,8 @@ export class TasksComponent implements OnInit{
 
   // It is not needed because the getTasks returns an observable, so each change done in the list of tasks
   // that is done is propagated to its subscribers - in this case the getTasks from task-list component
-  refreshList() {
-    this.taskService.getTasks()
-      .subscribe(t => this.taskList = t);
-  }
+  // refreshList() {
+  //   this.taskService.getTasks()
+  //     .subscribe(t => this.taskList = t);
+  // }
 }
